6ebb0ab8b2bb1a1b2ac5598ef76ae7e7cb9e7acc
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-03-18 09:30:05 +0700

    Update code

--- lib/screen/nav_bar/page_list_view.dart
+++ lib/screen/nav_bar/page_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class PageListView extends StatefulWidget {
   const PageListView({super.key});
@@ -8,8 +9,103 @@ class PageListView extends StatefulWidget {
 }
 
 class _PageListViewState extends State<PageListView> {
+  //Data List Berita
+  List<Map<String, dynamic>> listBerita = [
+    {
+      "judul": "Judul Berita satu",
+      "isi":
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dign",
+      "tanggal": "15 Maret 2025",
+      "gambar": "gambar/berita1.jpeg",
+      "rating": 5,
+    },
+    {
+      "judul": "Judul Berita dua",
+      "isi":
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dign",
+      "tanggal": "16 Maret 2025",
+      "gambar": "gambar/berita2.jpeg",
+      "rating": 4,
+    },
+    {
+      "judul": "Judul Berita tiga",
+      "isi":
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dign",
+      "tanggal": "17 Maret 2025",
+      "gambar": "gambar/berita3.jpeg",
+      "rating": 3,
+    },
+    {
+      "judul": "Judul Berita empat",
+      "isi":
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dign",
+      "tanggal": "17 Maret 2025",
+      "gambar": "gambar/berita4.jpg",
+      "rating": 2,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: listBerita.length,
+          itemBuilder: (context, index) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(
+                        listBerita[index]['gambar'],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          listBerita[index]['judul'],
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(listBerita[index]['tanggal']),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            RatingBarIndicator(
+                              rating: listBerita[index]['rating'],
+                              itemBuilder:
+                                  (context, index) =>
+                                  Icon(Icons.star, color: Colors.yellow),
+                              itemCount: 5,
+                              itemSize: 15,
+                            ),
+                            SizedBox(width: 20,)
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
